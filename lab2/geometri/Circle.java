27fbeaac5248376291534cf3a0aa00460b903dcb
John Andersson <exodusx32@gmail.com>
2015-11-20 12:59:10 +0100

    a

--- lab2/geometri/Circle.java
+++ lab2/geometri/Circle.java
@@ -35,11 +35,29 @@ public class Circle implements GeometricalForm{
 	}
     public void move(int dx, int dy) throws IllegalPositionException{
 	}
+
+	/**
+	 *
+	 * @param x
+	 * @param y
+	 * @throws IllegalPositionException
+	 */
     public void place(int x, int y) throws IllegalPositionException{
 	}
+
+	/**
+	 * Indicates whether some other object is "equal to" this one, sharing all attributes except for position.
+	 * @param o object to compare.
+	 * @return
+	 */
 	@Override
 	public boolean equals(Object o){
 	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 * @return a hash code value for this object.
+	 */
 	@Override
 	public int hashCode(){
 	}
--- lab2/geometri/GeometricalForm.java
+++ lab2/geometri/GeometricalForm.java
@@ -11,4 +11,19 @@ import java.awt.*;
         public int getY();
         public void move(int dx, int dy) throws IllegalPositionException;
         public void place(int x, int y) throws IllegalPositionException;
+
+	    /**
+	     * Indicates whether some other object is "equal to" this one, sharing all attributes except for position.
+	     * @param o object to compare.
+	     * @return
+	     */
+	    @Override
+	    public boolean equals(Object o);
+
+	    /**
+	     * Returns a hash code value for the object.
+	     * @return a hash code value for this object.
+	     */
+	    @Override
+	    public int hashCode();
     }
