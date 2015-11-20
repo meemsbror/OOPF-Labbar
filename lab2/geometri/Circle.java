d480ce4645e3d46109cb498c640f494674b405c3
John Andersson <exodusx32@gmail.com>
2015-11-20 12:43:12 +0100

    arst

--- lab2/geometri/Circle.java
+++ lab2/geometri/Circle.java
@@ -1,32 +1,41 @@
+package geometri;
+
+import java.awt.*;
+import java.util.Objects;
+
 public class Circle implements GeometricalForm{
 	
-	int x,y,diameter;
-	Color color;
+	private int x,y,diameter;
+	private Color color;
 	
 	public Circle(int x, int y, int d,Color c){
 	}
 
 	public Circle(GeometricalForm f, int d, Color c){
 	}
+
+	@Override
+	public int compareTo(GeometricalForm g) {
+	}
 	public void fill(Graphics g){
 	}
-        public Color getColor(){
+	public Color getColor(){
 	}
-        public int getArea(){
+    public int getArea(){
 	}
-        public int getHeight(){
+    public int getHeight(){
 	}
-        public int getPerimeter(){
+    public int getPerimeter(){
 	}
-        public int getWidth(){
+    public int getWidth(){
 	}
-        public int getX(){
+    public int getX(){
 	}
-        public int getY(){
+    public int getY(){
 	}
-        public void move(int dx, int dy) throws IllegalPositionException{
+    public void move(int dx, int dy) throws IllegalPositionException{
 	}
-        public void place(int x, int y) throws IllegalPositionException{
+    public void place(int x, int y) throws IllegalPositionException{
 	}
 	@Override
 	public boolean equals(Object o){
