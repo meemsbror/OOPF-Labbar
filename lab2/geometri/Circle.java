b48bd3372b629227600e6b1a769f4f3723fc6c33
John Andersson <exodusx32@gmail.com>
2015-11-24 17:10:23 +0100

    nej

--- lab2/geometri/Circle.java
+++ lab2/geometri/Circle.java
@@ -2,7 +2,7 @@ package geometri;
 
 import java.awt.*;
 
-public class Circle implements GeometricalForm{
+public class Circle extends AbstractGeometricalForm {
 	
 	private int x,y,diameter;
 	private Color color;
@@ -26,47 +26,5 @@ public class Circle implements GeometricalForm{
 	public Circle(GeometricalForm f, int d, Color c){
 	}
 
-	@Override
-	public int compareTo(GeometricalForm f) {
-		return 0;
-	}
-	public void fill(Graphics g){
-	}
-	public Color getColor(){
 
-		return Color.red;
-	}
-    public int getArea(){
-		return 0;
-	}
-    public int getHeight(){
-		return 0;
-	}
-    public int getPerimeter(){
-		return 0;
-	}
-    public int getWidth(){
-		return 0;
-	}
-    public int getX(){
-		return 0;
-	}
-    public int getY(){
-		return 0;
-	}
-    public void move(int dx, int dy) throws IllegalPositionException{
-	}
-
-    public void place(int x, int y) throws IllegalPositionException{
-	}
-
-	@Override
-	public boolean equals(Object o){
-		return false;
-	}
-
-	@Override
-	public int hashCode(){
-		return 0;
-	}
 }
--- lab2/geometri/Point.java
+++ lab2/geometri/Point.java
@@ -3,7 +3,7 @@ package geometri;
 import java.awt.*;
 import java.util.Objects;
 
-public class Point implements GeometricalForm{
+public class Point extends AbstractGeometricalForm {
 
     private int x,y;
     private Color color;
@@ -25,47 +25,5 @@ public class Point implements GeometricalForm{
     public Point(GeometricalForm f, Color c){
     }
 
-    @Override
-    public int compareTo(GeometricalForm f) {
-        return 0;
-    }
-    public void fill(Graphics g){
-    }
-    public Color getColor(){
 
-        return Color.red;
-    }
-    public int getArea(){
-        return 0;
-    }
-    public int getHeight(){
-        return 0;
-    }
-    public int getPerimeter(){
-        return 0;
-    }
-    public int getWidth(){
-        return 0;
-    }
-    public int getX(){
-        return 0;
-    }
-    public int getY(){
-        return 0;
-    }
-    public void move(int dx, int dy) throws IllegalPositionException{
-    }
-
-    public void place(int x, int y) throws IllegalPositionException{
-    }
-
-    @Override
-    public boolean equals(Object o){
-        return false;
-    }
-
-    @Override
-    public int hashCode(){
-        return 0;
-    }
 }
