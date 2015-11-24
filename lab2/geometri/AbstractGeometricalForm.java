daf3d58bed4df5d6c7e6dc244d8899a5944b3a6f
Frej Karlsson <frejlekarlsson@gmail.com>
2015-11-24 17:08:11 +0100

    yolo say nolo

---
+++ lab2/geometri/AbstractGeometricalForm.java
@@ -0,0 +1,51 @@
+package geometri;
+
+
+import java.awt.*;
+
+public abstract class AbstractGeometricalForm implements GeometricalForm{
+    @Override
+    public int compareTo(GeometricalForm f) {
+        return 0;
+    }
+    public void fill(Graphics g){
+    }
+    public Color getColor(){
+
+        return Color.red;
+    }
+    public int getArea(){
+        return 0;
+    }
+    public int getHeight(){
+        return 0;
+    }
+    public int getPerimeter(){
+        return 0;
+    }
+    public int getWidth(){
+        return 0;
+    }
+    public int getX(){
+        return 0;
+    }
+    public int getY(){
+        return 0;
+    }
+    public void move(int dx, int dy) throws IllegalPositionException{
+    }
+
+    public void place(int x, int y) throws IllegalPositionException{
+    }
+
+    @Override
+    public boolean equals(Object o){
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        return 0;
+    }
+}
+}
