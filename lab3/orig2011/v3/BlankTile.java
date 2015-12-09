5a4513894cc382fd0ab1085107d080a0db3c6098
John Andersson <exodusx32@gmail.com>
2015-12-09 12:13:30 +0100

    gjorde och lade till v3

---
+++ lab3/orig2011/v3/BlankTile.java
@@ -0,0 +1,13 @@
+package orig2011.v3;
+
+import java.awt.*;
+
+public class BlankTile implements GameTile{
+
+	public void draw(Graphics g, int x, int y, Dimension d){
+		// The default GameTile is transparent,
+		// therefore no drawing is performed.
+	}
+
+
+}
