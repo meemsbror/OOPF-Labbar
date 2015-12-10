a7f1789abf3f35568de8cda7d808ff7554915dde
John Andersson <exodusx32@gmail.com>
2015-12-10 15:28:26 +0100

    vi har bra commits

--- lab3/orig2011/v3/BlankTile.java
+++ lab3/orig2011/v3/BlankTile.java
@@ -2,9 +2,6 @@ package orig2011.v3;
 
 import java.awt.*;
 
-/**
- * Created by frej on 12/9/15.
- */
 public class BlankTile implements GameTile{
 
     public void draw(Graphics g, int x, int y, Dimension d){
---
+++ lab3/orig2011/v4/BlankTile.java
@@ -0,0 +1,11 @@
+package orig2011.v4;
+
+import java.awt.*;
+
+public class BlankTile implements GameTile {
+
+    public void draw(Graphics g, int x, int y, Dimension d){
+        // The BlankTile is transparent,
+        // therefore no drawing is performed.
+    }
+}
